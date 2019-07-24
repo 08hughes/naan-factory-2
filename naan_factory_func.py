3328b8a9cfbf57884b7eb8097fc4215ce4913f15
Daniel Hughes <DHughes@spartaglobal.com>
2019-07-24 16:53:35 +0100

    test 1 and 2 satisfied

--- naan_factory_func.py
+++ naan_factory_func.py
@@ -1,2 +1,5 @@
-def make_dough(arg1, arg2):
-    return "dough"
+def make_dough(ingredient1, ingredient2):
+    if ingredient1 == "wheat" and ingredient2 == "water" or ingredient2 == "wheat" and ingredient1 == "water":
+        return "dough"
+    else:
+        return "none"
--- naan_tests.py
+++ naan_tests.py
@@ -2,7 +2,10 @@ from naan_factory_func import *
 # Test 1 - making_dough() input - wheat and water - expected output dough
 print(make_dough("wheat", "water") == "dough")
 
-# Test 2 - bake_dough() input = dough expected output = naan
+# Test 2 make_dough - input = x and y - expected output = none?
+print(make_dough("x", "y") == "none")
+
+# Test 3 - bake_dough() input = dough expected output = naan
 
 
 #
